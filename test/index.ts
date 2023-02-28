af38af7c03e324d24f7a2f6d2ee5ed27c296b7df
Žiga Leber <zigaleber7@gmail.com>
2023-02-28 11:21:20 +0100

    Make the test run on Node 14
    This does not effect the package, as tests are not included.

--- test/index.ts
+++ test/index.ts
@@ -1,9 +1,9 @@
 import test from 'ava';
 import fs from 'node:fs';
 import {parser} from '../src/index.js';
-import aai from './aai.json' assert { type: "json" };
 
 test('aai', async t => {
-  const stream = fs.createReadStream('./test/examples/aai.xml');
-  t.deepEqual(await parser(stream), aai);
+  const expected = JSON.parse(fs.readFileSync('./test/aai.json', 'utf-8'));
+  const stream = fs.createReadStream('./test/aai.xml');
+  t.deepEqual(await parser(stream), expected);
 });
