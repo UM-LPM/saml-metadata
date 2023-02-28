87e127684f26651dda3b693ad6f62a6b7001f966
Žiga Leber <zigaleber7@gmail.com>
2023-02-28 12:44:25 +0100

    Add custom error, export types

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,360 +1,34 @@
 import {Stream, Readable} from 'node:stream';
 import sax from 'saxes';
 
-// An incomplete implementation of the SAML metadata schema
+import {
+  ParseError,
+  Localized,
+  Attribute,
+  Endpoint,
+  IndexedEndpoint,
+  KeyType,
+  Key,
+  EntitiesDescriptor,
+  EntityDescriptor,
+  Organization,
+  ContactType,
+  ContactPerson,
+  Role,
+  IDPSSO,
+  RequestedAttribute,
+  AttributeConsumingService,
+  SPSSO,
+  Metadata
+} from './types.js';
+import * as p from './path.js';
 
-// <complexType name="localizedNameType">
-//     <simpleContent>
-//         <extension base="string">
-//             <attribute ref="xml:lang" use="required"/>
-//         </extension>
-//     </simpleContent>
-// </complexType>
-// <complexType name="localizedURIType">
-//     <simpleContent>
-//         <extension base="anyURI">
-//             <attribute ref="xml:lang" use="required"/>
-//         </extension>
-//     </simpleContent>
-// </complexType>
-type Localized = {lang: string; content: string};
-
-// <complexType name="AttributeType">
-//     <sequence>
-//         <element ref="saml:AttributeValue" minOccurs="0" maxOccurs="unbounded"/>
-//     </sequence>
-//     <attribute name="Name" type="string" use="required"/>
-//     <attribute name="NameFormat" type="anyURI" use="optional"/>
-//     <attribute name="FriendlyName" type="string" use="optional"/>
-//     <anyAttribute namespace="##other" processContents="lax"/>
-// </complexType>
-// <element name="AttributeValue" type="anyType" nillable="true"/>
-interface Attribute {
-  name: string; // required
-  nameFormat: string | null;
-  friendlyName: string | null;
-  values: string[]
-}
-
-// <complexType name="EndpointType">
-//     <sequence>
-//         <any namespace="##other" processContents="lax" minOccurs="0" maxOccurs="unbounded"/>
-//     </sequence>
-//     <attribute name="Binding" type="anyURI" use="required"/>
-//     <attribute name="Location" type="anyURI" use="required"/>
-//     <attribute name="ResponseLocation" type="anyURI" use="optional"/>
-//     <anyAttribute namespace="##other" processContents="lax"/>
-// </complexType>
-interface Endpoint {
-  binding: string;
-  location: string;
-  responseLocation: string | null;
-}
-    
-// <complexType name="IndexedEndpointType">
-//     <complexContent>
-//         <extension base="md:EndpointType">
-//             <attribute name="index" type="unsignedShort" use="required"/>
-//             <attribute name="isDefault" type="boolean" use="optional"/>
-//         </extension>
-//     </complexContent>
-// </complexType>
-interface IndexedEndpoint extends Endpoint {
-  index: number;
-  isDefault: boolean | null;
-}
-
-// <complexType name="KeyDescriptorType">
-//     <sequence>
-//         <element ref="ds:KeyInfo"/>
-//         <element ref="md:EncryptionMethod" minOccurs="0" maxOccurs="unbounded"/>
-//     </sequence>
-//     <attribute name="use" type="md:KeyTypes" use="optional"/>
-// </complexType>
-// <simpleType name="KeyTypes">
-//     <restriction base="string">
-//         <enumeration value="encryption"/>
-//         <enumeration value="signing"/>
-//     </restriction>
-// </simpleType>
-// <element name="EncryptionMethod" type="xenc:EncryptionMethodType"/>
-type KeyType = 'encryption' | 'signing';
-interface Key {
-  certificates: string[];
-  //EncryptionMethod
-
-  use: KeyType | null
-}
-
-// <complexType name="EntitiesDescriptorType">
-//     <sequence>
-//         <element ref="ds:Signature" minOccurs="0"/>
-//         <element ref="md:Extensions" minOccurs="0"/>
-//         <choice minOccurs="1" maxOccurs="unbounded">
-//             <element ref="md:EntityDescriptor"/>
-//             <element ref="md:EntitiesDescriptor"/>
-//         </choice>
-//     </sequence>
-//     <attribute name="validUntil" type="dateTime" use="optional"/>
-//     <attribute name="cacheDuration" type="duration" use="optional"/>
-//     <attribute name="ID" type="ID" use="optional"/>
-//     <attribute name="Name" type="string" use="optional"/>
-// </complexType>
-interface EntitiesDescriptor {
-  // Signature
-  // Extensions
-  entities: (EntityDescriptor | EntitiesDescriptor)[], // required
-
-  validUntil: string | null; // Date
-  cacheDuration: string | null; // Duration
-  // ID
-  name: string | null;
-}
-
-// <complexType name="EntityDescriptorType">
-//     <sequence>
-//         <element ref="ds:Signature" minOccurs="0"/>
-//         <element ref="md:Extensions" minOccurs="0"/>
-//         <choice>
-//             <choice maxOccurs="unbounded">
-//                 <element ref="md:RoleDescriptor"/>
-//                 <element ref="md:IDPSSODescriptor"/>
-//                 <element ref="md:SPSSODescriptor"/>
-//                 <element ref="md:AuthnAuthorityDescriptor"/>
-//                 <element ref="md:AttributeAuthorityDescriptor"/>
-//                 <element ref="md:PDPDescriptor"/>
-//             </choice>
-//             <element ref="md:AffiliationDescriptor"/>
-//         </choice>
-//         <element ref="md:Organization" minOccurs="0"/>
-//         <element ref="md:ContactPerson" minOccurs="0" maxOccurs="unbounded"/>
-//         <element ref="md:AdditionalMetadataLocation" minOccurs="0" maxOccurs="unbounded"/>
-//     </sequence>
-//     <attribute name="entityID" type="md:entityIDType" use="required"/>
-//     <attribute name="validUntil" type="dateTime" use="optional"/>
-//     <attribute name="cacheDuration" type="duration" use="optional"/>
-//     <attribute name="ID" type="ID" use="optional"/>
-//     <anyAttribute namespace="##other" processContents="lax"/>
-// </complexType>
-interface EntityDescriptor {
-    entityID: string;
-    validUntil: string | null; // Date
-    cacheDuration: string | null; // Duration
-    // ID
-
-    // Signature
-    // Extensions
-    // RoleDescriptor
-    // AuthnAuthorityDescriptor
-    // AttributeAuthorityDescriptor
-    // PDPDescriptor
-    // AffiliationDescriptor
-    idps: IDPSSO[];
-    sps: SPSSO[];
-    organization: Organization | null;
-    contactPersons: ContactPerson[];
-    // AdditionalMetadataLocation
-}
-
-// <complexType name="OrganizationType">
-//     <sequence>
-//         <element ref="md:Extensions" minOccurs="0"/>
-//         <element ref="md:OrganizationName" maxOccurs="unbounded"/>
-//         <element ref="md:OrganizationDisplayName" maxOccurs="unbounded"/>
-//         <element ref="md:OrganizationURL" maxOccurs="unbounded"/>
-//     </sequence>
-//     <anyAttribute namespace="##other" processContents="lax"/>
-// </complexType>
-// <element name="OrganizationName" type="md:localizedNameType"/>
-// <element name="OrganizationDisplayName" type="md:localizedNameType"/>
-// <element name="OrganizationURL" type="md:localizedURIType"/>
-interface Organization {
-  // Extensions
-  organizationName: Localized[];
-  organizationDisplayName: Localized[];
-  organizationURL: Localized[];
-}
-
-// <simpleType name="ContactTypeType">
-//     <restriction base="string">
-//         <enumeration value="technical"/>
-//         <enumeration value="support"/>
-//         <enumeration value="administrative"/>
-//         <enumeration value="billing"/>
-//         <enumeration value="other"/>
-//     </restriction>
-// </simpleType>
-type ContactType = 'technical' | 'support' | 'administrative' | 'billing' | 'other';
-
-// <element name="ContactPerson" type="md:ContactType"/>
-// <complexType name="ContactType">
-//     <sequence>
-//         <element ref="md:Extensions" minOccurs="0"/>
-//         <element ref="md:Company" minOccurs="0"/>
-//         <element ref="md:GivenName" minOccurs="0"/>
-//         <element ref="md:SurName" minOccurs="0"/>
-//         <element ref="md:EmailAddress" minOccurs="0" maxOccurs="unbounded"/>
-//         <element ref="md:TelephoneNumber" minOccurs="0" maxOccurs="unbounded"/>
-//     </sequence>
-//     <attribute name="contactType" type="md:ContactTypeType" use="required"/>
-//     <anyAttribute namespace="##other" processContents="lax"/>
-// </complexType>
-// <element name="Company" type="string"/>
-// <element name="GivenName" type="string"/>
-// <element name="SurName" type="string"/>
-// <element name="EmailAddress" type="anyURI"/>
-// <element name="TelephoneNumber" type="string"/>
-interface ContactPerson {
-  // Extensions
-  company: string | null;
-  givenName: string | null;
-  surName: string | null;
-  emailAddresses: string[];
-  telephoneNumbers: string[];
-  contactType: ContactType; // required
-}
-
-// <complexType name="RoleDescriptorType" abstract="true">
-//     <sequence>
-//         <element ref="ds:Signature" minOccurs="0"/>
-//         <element ref="md:Extensions" minOccurs="0"/>
-//         <element ref="md:KeyDescriptor" minOccurs="0" maxOccurs="unbounded"/>
-//         <element ref="md:Organization" minOccurs="0"/>
-//         <element ref="md:ContactPerson" minOccurs="0" maxOccurs="unbounded"/>
-//     </sequence>
-//     <attribute name="ID" type="ID" use="optional"/>
-//     <attribute name="validUntil" type="dateTime" use="optional"/>
-//     <attribute name="cacheDuration" type="duration" use="optional"/>
-//     <attribute name="protocolSupportEnumeration" type="md:anyURIListType" use="required"/>
-//     <attribute name="errorURL" type="anyURI" use="optional"/>
-//     <anyAttribute namespace="##other" processContents="lax"/>
-// </complexType>
-interface Role {
-    // Signature
-    // Extensions
-    keys: Key[];
-    organization: Organization | null;
-    contactPersons: ContactPerson[];
-
-    // ID
-    validUntil: string | null; // Date
-    cacheDuration: string | null; // Duration
-    protocolSupportEnumeration: string[];
-    errorURL: string | null;
-}
-
-// <complexType name="SSODescriptorType" abstract="true">
-//     <complexContent>
-//         <extension base="md:RoleDescriptorType">
-//             <sequence>
-//                 <element ref="md:ArtifactResolutionService" minOccurs="0" maxOccurs="unbounded"/>
-//                 <element ref="md:SingleLogoutService" minOccurs="0" maxOccurs="unbounded"/>
-//                 <element ref="md:ManageNameIDService" minOccurs="0" maxOccurs="unbounded"/>
-//                 <element ref="md:NameIDFormat" minOccurs="0" maxOccurs="unbounded"/>
-//             </sequence>
-//         </extension>
-//     </complexContent>
-// </complexType>
-// <element name="ArtifactResolutionService" type="md:IndexedEndpointType"/>
-// <element name="SingleLogoutService" type="md:EndpointType"/>
-// <element name="ManageNameIDService" type="md:EndpointType"/>
-// <element name="NameIDFormat" type="anyURI"/>
-interface SSO extends Role {
-  // ArtifactResolutionService
-  singleLogoutServices: Endpoint[];
-  // ManageNameIDService
-  // NameIDFormat
-}
-
-// <complexType name="IDPSSODescriptorType">
-//     <complexContent>
-//         <extension base="md:SSODescriptorType">
-//             <sequence>
-//                 <element ref="md:SingleSignOnService" maxOccurs="unbounded"/>
-//                 <element ref="md:NameIDMappingService" minOccurs="0" maxOccurs="unbounded"/>
-//                 <element ref="md:AssertionIDRequestService" minOccurs="0" maxOccurs="unbounded"/>
-//                 <element ref="md:AttributeProfile" minOccurs="0" maxOccurs="unbounded"/>
-//                 <element ref="saml:Attribute" minOccurs="0" maxOccurs="unbounded"/>
-//             </sequence>
-//             <attribute name="WantAuthnRequestsSigned" type="boolean" use="optional"/>
-//         </extension>
-//     </complexContent>
-// </complexType>
-// <element name="SingleSignOnService" type="md:EndpointType"/>
-// <element name="NameIDMappingService" type="md:EndpointType"/>
-// <element name="AssertionIDRequestService" type="md:EndpointType"/>
-// <element name="AttributeProfile" type="anyURI"/>
-interface IDPSSO extends SSO {
-  singleSignOnServices: Endpoint[]; // required
-  // NameIDMappingService
-  // AssertionIDRequestService
-  // AttributeProfile
-  // Attribute
-
-  wantAuthnRequestsSigned: boolean | null;
-}
-
-// <complexType name="RequestedAttributeType">
-//     <complexContent>
-//         <extension base="saml:AttributeType">
-//             <attribute name="isRequired" type="boolean" use="optional"/>
-//         </extension>
-//     </complexContent>
-// </complexType>
-interface RequestedAttribute extends Attribute {
-  isRequired: boolean | null;
-}
-
-// <complexType name="AttributeConsumingServiceType">
-//     <sequence>
-//         <element ref="md:ServiceName" maxOccurs="unbounded"/>
-//         <element ref="md:ServiceDescription" minOccurs="0" maxOccurs="unbounded"/>
-//         <element ref="md:RequestedAttribute" maxOccurs="unbounded"/>
-//     </sequence>
-//     <attribute name="index" type="unsignedShort" use="required"/>
-//     <attribute name="isDefault" type="boolean" use="optional"/>
-// </complexType>
-// <element name="ServiceName" type="md:localizedNameType"/>
-// <element name="ServiceDescription" type="md:localizedNameType"/>
-// <element name="RequestedAttribute" type="md:RequestedAttributeType"/>
-interface AttributeConsumingService {
-  serviceName: Localized[];
-  serviceDescription: Localized[];
-  requestedAttributes: RequestedAttribute[];
-
-  index: number;
-  isDefault: boolean | null;
-}
-
-// <complexType name="SPSSODescriptorType">
-//     <complexContent>
-//         <extension base="md:SSODescriptorType">
-//             <sequence>
-//                 <element ref="md:AssertionConsumerService" maxOccurs="unbounded"/>
-//                 <element ref="md:AttributeConsumingService" minOccurs="0" maxOccurs="unbounded"/>
-//             </sequence>
-//             <attribute name="AuthnRequestsSigned" type="boolean" use="optional"/>
-//             <attribute name="WantAssertionsSigned" type="boolean" use="optional"/>
-//         </extension>
-//     </complexContent>
-// </complexType>
-// <element name="AssertionConsumerService" type="md:IndexedEndpointType"/>
-// <element name="AttributeConsumingService" type="md:AttributeConsumingServiceType"/>
-interface SPSSO extends SSO {
-  assertionConsumerServices: IndexedEndpoint[]; // required
-  attributeConsumingServices: AttributeConsumingService[];
-
-  authnRequestsSigned: boolean | null;
-  wantAssertionsSigned: boolean | null;
-}
-
-type Metadata = EntityDescriptor | EntitiesDescriptor;
-    
 function parseBoolean(s: string | null): boolean | null {
   switch(s) {
+    case null: return null;
     case 'true': return true;
     case 'false': return false;
-    default: return null;
+    default: throw new ParseError(`Boolean expected: "${s}"`)
   }
 }
 
@@ -363,15 +37,20 @@ function parseInteger(s: string | null): number | null {
     return null;
   }
   const n = parseInt(s, 10);
-  return isNaN(n) ? null : n;
+  if (isNaN(n)) {
+    throw new ParseError(`Integer expected: "${s}"`);
+  } else {
+    return n;
+  }
 }
 
 function parseUse(s: string | null): KeyType | null {
   switch(s) {
+    case null: return null;
     case 'encryption':
     case 'signing':
       return s as KeyType;
-    default: return null;
+    default: throw new ParseError(`Invalid key type: "${s}"`);
   }
 }
 
@@ -390,134 +69,9 @@ function parseContactType(s: string): ContactType {
     case 'billing':
     case 'other':
       return s as ContactType;
-    default: throw new Error('Invalid contact type.');
+    default: throw new ParseError(`Invalid contact type: "${s}"`);
   }
 }
-
-namespace Path {
-  export interface Matcher {
-    match(top: Tag[], k: (top_: Tag[]) => boolean): boolean
-  }
-
-  class Tag implements Matcher {
-    ns: string | undefined;
-    local: string;
-
-    constructor(ns: string | undefined, local: string) {
-      this.ns = ns;
-      this.local = local;
-    }
-
-    equal(other: Tag) {
-      return this.ns === other.ns && this.local === other.local;
-    }
-
-    match(top: Tag[], k: (top_: Tag[]) => boolean): boolean {
-      return top.length === 0 
-        ? false 
-        : top[top.length - 1].equal(this) && k(top.slice(0, -1));
-    }
-  }
-
-
-  class And implements Matcher {
-    left: Matcher;
-    right: Matcher;
-
-    constructor(left: Matcher, right: Matcher) {
-      this.left = left;
-      this.right = right;
-    }
-
-    match(top: Tag[], k: (top_: Tag[]) => boolean): boolean {
-      return this.right.match(top, (top_) => this.left.match(top_, k)); // Match in reverse
-    }
-  }
-
-  class Or implements Matcher {
-    ms: Matcher[];
-
-    constructor(ms: Matcher[]) {
-      this.ms = ms;
-    }
-
-    match(top: Tag[], k: (top_: Tag[]) => boolean): boolean {
-      return this.ms.some((m) => m.match(top, k));
-    }
-  }
-
-  class Repeat implements Matcher {
-    m: Matcher;
-
-    constructor(m: Matcher) {
-      this.m = m; 
-    }
-
-    matchRepeatedly(top: Tag[], k: (top_: Tag[]) => boolean): boolean {
-      return k(top) || this.m.match(top, (top_) => top !== top_ && this.matchRepeatedly(top_, k));
-    }
-
-    match(top: Tag[], k: (top_: Tag[]) => boolean): boolean {
-      return this.matchRepeatedly(top, k);
-    }
-  }
-
-  class Empty implements Matcher {
-    match(top: Tag[], k: (top_: Tag[]) => boolean): boolean {
-      return k(top);
-    }
-  }
-
-  class Root implements Matcher {
-    match(top: Tag[], k: (top_: Tag[]) => boolean): boolean {
-      return top.length === 0;
-    }
-  }
-
-  export function or(ms: Matcher[]) {
-    return new Or(ms);
-  }
-
-  export function and(left: Matcher, right: Matcher) {
-    return new And(left, right);
-  }
-
-  export function tag(ns: string | undefined, tag: string) {
-    return new Tag(ns, tag);
-  }
-
-  export function repeat(m: Matcher) {
-    return new Repeat(m);
-  }
-
-  export const root = new Root();
-
-  export const empty = new Empty();
-
-  export class Stack {
-    tags: Tag[] = [];
-
-    push(ns: string | undefined, local: string) {
-      this.tags.push(new Tag(ns, local));
-    }
-
-    pop(ns_: string | undefined, local_: string) {
-      const tag = this.tags.pop();
-      if (tag === undefined) {
-        throw Error('Stack empty'); 
-      }
-      if (!tag.equal(new Tag(ns_, local_))) {
-        throw Error(`Unexpected tag ${tag}`); 
-      }
-    }
-
-    match(m: Matcher): boolean {
-      return m.match(this.tags, (_) => true);
-    }
-  }
-}
-
-import p = Path
 
 export default async function(stream: Readable): Promise<Metadata | undefined> {
 
@@ -571,7 +125,7 @@ export default async function(stream: Readable): Promise<Metadata | undefined> {
       if (attribute) {
         return attribute.value;
       } else {
-        throw new Error(`The attribute "${name}" is required!`);
+        throw new ParseError(`The attribute "${name}" is required!`);
       }
     }
     function createSSO() {
@@ -997,7 +551,7 @@ export default async function(stream: Readable): Promise<Metadata | undefined> {
     nss.pop();
   });
   const end: Promise<Metadata | undefined> = new Promise((resolve, reject) => {
-    parser.on('end', () =>  resolve(metadata));
+    parser.on('end', () => resolve(metadata));
     parser.on('error', (e) => reject(e));
   });
 
