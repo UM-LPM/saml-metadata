674406a9ccb6a4bdf2d1aa2e294afe7b7aaeccd5
Žiga Leber <mail+github@zigaleber.com>
2023-12-14 14:25:42 +0100

    Use node test instead of ava

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,11 @@
-import test from 'ava';
+import test from 'node:test';
+import assert from 'node:assert';
 import fs from 'node:fs';
-import {classToPlain} from 'class-transformer';
+import {instanceToPlain} from 'class-transformer';
 import {parser} from '../src/index.js';
 
 test('aai', async t => {
   const expected = JSON.parse(fs.readFileSync('./test/aai.json', 'utf-8'));
   const stream = fs.createReadStream('./test/aai.xml');
-  t.deepEqual(classToPlain(await parser(stream)), expected);
+  assert.deepStrictEqual(instanceToPlain(await parser(stream)), expected);
 });
